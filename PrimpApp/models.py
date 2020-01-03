a40b0fb5bda8a7bbc659cac901709c49c9c8ac12
rjrobins16 <rjrobins16@gmail.com>
2020-01-03 12:53:11 -0600

    draft 1

--- PrimpApp/models.py
+++ PrimpApp/models.py
@@ -8,6 +8,7 @@ from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    title = models.CharField(max_length = 300, null = True)
     is_stylist = models.BooleanField(blank=True, default=False)
     latitude = models.DecimalField(max_digits = 10, decimal_places=8 , null=True, default=35.148272)
     longitude = models.DecimalField(max_digits = 10, decimal_places=8, null=True, default=-90.1108723)
@@ -16,7 +17,7 @@ class Profile(models.Model):
     Profile_Picture = models.ImageField(upload_to='media', null=True, blank=True)
 
     def __str__(self):
-        return f"(user: {self.user}, is_stylist:{self.is_stylist},latitude:{self.latitude} " \
+        return f"(user: {self.user}, Title:{self.title}, is_stylist:{self.is_stylist},latitude:{self.latitude} " \
                f" longitude:{self.longitude}, TypeofStylist:{self.TypeofStylist}, DateOfBirth:{self.DateOfBirth})"
 
 # @receiver(post_save, sender=User)
